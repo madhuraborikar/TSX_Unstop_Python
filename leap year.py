046cec99071aa90b12a982db1c650713ba4a4aff
Madhura Borikar <madhuraborikar531@gmail.com>
2025-05-16 20:16:05 +0530

    Added

---
+++ leap year.py
@@ -0,0 +1,17 @@
+'''Write a script that checks if a given year is a leap year (divisible by 
+4, but not by 100 unless also divisible by 400). 1'''
+
+def is_leap_year(year):
+    # Leap year if divisible by 4 and (not divisible by 100 or divisible by 400)
+    if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+        return True
+    else:
+        return False
+
+# Example usage
+year = int(input("Enter a year: "))
+
+if is_leap_year(year):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
